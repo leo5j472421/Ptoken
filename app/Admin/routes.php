5f7b6eb4ac32962f85ab7ee69999792f6c750004
N.M <freedomtomdestiny@gmail.com>
2018-05-22 21:20:46 +0800

    User & Transaction controller added

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -11,5 +11,7 @@ Route::group([
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index');
+    $router->resource('users', UserController::class);
+    $router->resource('transactions', TransactionController::class);
 
 });
